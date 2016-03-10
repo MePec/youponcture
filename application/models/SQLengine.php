4ea86446fd3464953ba64323b4d1c817b85367f1
Tof69 <christophe.cmr@gmail.com>
2016-03-10 14:22:54 -0800

    Ajout fichier SQLengine.php

---
+++ application/models/SQLengine.php
@@ -0,0 +1,21 @@
+<?php
+
+// 1ère fonction de Test PDO pour requete SQL
+/* fonction getSymptoms()
+ Description : permet de récupérer la liste de toutes les pathologies de la BDD
+ Paramètres : ...
+*/
+function getPathos() {
+$sql = "SELECT * FROM patho ;";
+
+$querry = $dbh->prepare($sql);
+$querry->execute();
+
+$result = $querry->fetch(PDO::FETCH_ASSOC);
+
+return $result;
+}
+
+
+
+?>
