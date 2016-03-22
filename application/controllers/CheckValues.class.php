064442cd7f1ef1a7c64fc9120e11a16e7c64f5c2
Maxb018 <Maxb018@users.noreply.github.com>
2016-03-22 17:57:22 +0100

    Ajout checkEmail

--- application/controllers/CheckValues.class.php
+++ application/controllers/CheckValues.class.php
@@ -2,6 +2,7 @@
 <?php
 class CheckValues {
 
+    // Le login peut contenir des caractères alphaumériques et des tirets unique, sa longueur est comprise entre 3 et 15.
     function checkLogin($login) {
         if (preg_match ( '/^[\w\d-]{3,15}$/', $login)){
             return true;
@@ -10,11 +11,32 @@ class CheckValues {
         }
     }
 
+
+    // Le password doit faire 8 caractères minimum, contenir au moins une majuscule, une miniscule et un chiffre, tout les caractères sont autorisés.
     function checkPwd($pwd) {
         if (preg_match ( '/^\S*(?=\S{8,})(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[\d])\S*$/', $pwd)){
             return true;
         } else {
             return false;
+        }
+    }
+
+    // Verifie la syntaxe de l'adresse mail et l'existance du MX du domaine
+    // Retourne 0, si la syntaxe est mauvaise,
+    // et 2, si le MX n'existe pas.
+    function checkEmail($email) {
+
+        $domain = NULL;
+
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $domain = substr(strrchr($email, "@"), 1);
+            if(checkdnsrr($domain, 'MX')){
+                return 1;
+            } else {
+                return 2;
+            }
+        } else {
+            return 0;
         }
     }
 }
--- tests/testCheck.php
+++ tests/testCheck.php
@@ -2,8 +2,9 @@
 
 require_once("../application/controllers/CheckValues.class.php");
 
-print "Login ?: ".CheckValues::checkLogin(" Maxb018")."<br/>";
-print "Pwd ?: ".CheckValues::checkPwd("Test19ce");
+print "Login ?: ".CheckValues::checkLogin("Maxb018")."<br/>";
+print "Pwd ?: ".CheckValues::checkPwd("Test19ce")."<br/>";
+print "E-mail ?: ".CheckValues::checkEmail("maxime.brunet@cpe.fr")."<br/>";
 
 
 ?>
